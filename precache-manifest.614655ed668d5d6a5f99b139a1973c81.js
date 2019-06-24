901dff48ef7b4baa6d41161a3cff29b5a976e386
jakubk3697 <kupson3697@gmail.com>
2019-06-24 22:14:16 +0200

    Updates

---
+++ precache-manifest.614655ed668d5d6a5f99b139a1973c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb10486be4d876f83af3255d0519aec2",
+    "url": "/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "66601c758597e2c511a0",
+    "url": "/React-To-Do-List/static/css/main.eee13c0f.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/React-To-Do-List/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "66601c758597e2c511a0",
+    "url": "/React-To-Do-List/static/js/main.8bab2778.chunk.js"
+  },
+  {
+    "revision": "8f7a86cad78759fc5d4e",
+    "url": "/React-To-Do-List/static/js/runtime~main.5a408617.js"
+  },
+  {
+    "revision": "53aae0972131fcadfdc753c2c92e6a9f",
+    "url": "/React-To-Do-List/static/media/background.53aae097.jpg"
+  }
+]);
